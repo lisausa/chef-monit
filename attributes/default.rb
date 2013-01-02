41c53b7e4f81b2566a33e196fc31b6b67920947e
Ryan Taylor Long <ryan@rtlong.com>
2013-01-02 11:49:39 -0600

    Add reminder alert

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,10 @@
 default[:monit][:enable_email]          = true
 
-default[:monit][:notify_email]          = "notify@example.com"
-
 default[:monit][:poll_period]           = 60
 default[:monit][:poll_start_delay]      = 120
+
+default[:monit][:notify_email]          = "notify@example.com"
+default[:monit][:notify_reminder]       = 10 # remind every X minutes
 
 default[:monit][:mail_format][:subject] = "$SERVICE $EVENT"
 default[:monit][:mail_format][:from]    = "monit@#{node['fqdn']}"
@@ -29,9 +30,9 @@ default[:monit][:allow] = ['0.0.0.0/0']
 
 default[:monit][:check_system][:alerts] = [
   'loadavg (1min) > 4',
-  'loadavg (5min) > 2',
+  'loadavg (5min) > 4',
   'memory usage > 95%',
   'cpu usage (user) > 90%',
-  'cpu usage (system) > 30%',
-  'cpu usage (wait) > 20%'
+  'cpu usage (system) > 40%',
+  'cpu usage (wait) > 30%'
 ]
